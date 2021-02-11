8470a9e35d929531b4f17f09c387a3e02931103b
Julian Patterson <julian.patterson@gmail.com>
2021-02-10 23:01:35 -0500

    commit tree 7361034a9dde3b31dd59ac6da45ec273c123bb8e

--- nested.rb
+++ nested.rb
@@ -14,9 +14,8 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
-
+      }
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +52,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,8 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP","C"]}
+  programmer_hash
 end
 
 def changing_alan
@@ -101,8 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash     
 end
 
 def adding_to_dennis
@@ -121,5 +122,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  # The above is the same as the following:
+  # programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  # Because the value of [:language] is an array
+  programmer_hash
 
 end
